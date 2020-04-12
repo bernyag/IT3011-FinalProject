922104bae8fb5b6ad141608d4969875082b45c79
kiramolloy <kira.molloy@corptaxsolutions.com.au>
2020-04-13 00:01:18 +0800

    got input to work for cNN

--- run.py
+++ run.py
@@ -11,48 +11,82 @@ from sklearn.model_selection import train_test_split
 from keras.utils import np_utils
 import numpy as np
 import csv
+from matplotlib import pyplot as plt
+import cv2
+
+values=['div','times','+','-','0','1','2','3','4','5','6','7','8','9']
 
 # load dataset
-file = np.genfromtxt('good.csv', delimiter=',')
+file = np.genfromtxt('./equations/good.csv', delimiter=',')
 # file = csv.reader("good.csv", delimiter=',')
 data = file[1:, 1:]
+changedData=[]
+for img in data:
+	img=cv2.resize(img,(45,45))
+	img=img.flatten()/255
+	changedData.append(img)
+changedData=np.asarray(changedData)
+
 labellist = []
-with open('good.csv') as f:
+with open('./equations/good.csv') as f:
     reader = csv.reader(f, delimiter=",")
     for i in reader:
         labellist.append(list(i)[0])
 labels = np.asarray(labellist)[1:]
-print(data.shape)
-print(labels.shape)
-X_train, X_test, y_train, y_test = train_test_split(data, labels, shuffle=False)
-X_train = X_train.reshape((X_train.shape[0], 45, 45, 1)).astype('float32')
-X_test = X_test.reshape((X_test.shape[0], 45, 45, 1)).astype('float32')
+newLabels=[]
+for each in labels:
+	currLabel=values.index(each)
+	newLabels.append(currLabel)
+labels=np.asarray(newLabels)
+
+x_train, x_test, y_train, y_test = train_test_split(changedData, labels, test_size = 0.2, random_state = 101)
+x_train = x_train.reshape(x_train.shape[0], 45, 45)
+x_test = x_test.reshape(x_test.shape[0], 45, 45)
+plt.imshow(x_train[0])
+plt.title(y_train[0])
+plt.show()
+plt.imshow(x_train[1])
+plt.title(y_train[1])
+plt.show()
+plt.imshow(x_train[2])
+plt.title(y_train[2])
+plt.show()
+plt.imshow(x_train[3])
+plt.title(y_train[3])
+plt.show()
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
 # normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
+x_train = x_train / 255
+x_test = x_test / 255
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 num_classes = y_test.shape[1]
 print(num_classes)
 
-# define a simple CNN model
-def baseline_model():
-	# create model
-	model = Sequential()
-	model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation='relu'))
-	model.add(MaxPooling2D())
-	model.add(Dropout(0.2))
-	model.add(Flatten())
-	model.add(Dense(128, activation='relu'))
-	model.add(Dense(num_classes, activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
-# build the model
-model = baseline_model()
+model = Sequential()
+model.add(Conv2D(32, (5, 5), input_shape=(45, 45, 1), activation='relu'))
+model.add(MaxPooling2D())
+
+
+model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
 # Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200)
 # Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
+scores = model.evaluate(x_test, y_test, verbose=0)
 print("CNN Error: %.2f%%" % (100-scores[1]*100))
